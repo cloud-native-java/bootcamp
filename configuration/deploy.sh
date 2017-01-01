7c4e99dce448e3389b941b8eff6c5d03d206f45d
Josh Long <josh@joshlong.com>
2016-12-31 19:42:59 -0800

    adding PWS

---
+++ configuration/deploy.sh
@@ -0,0 +1,8 @@
+cf create-service p-mysql 100mb bootcamp-customers-mysql            # 1
+
+cf push -p target/configuration.jar bootcamp-customers \
+ --random-route --no-start                                          # 2
+
+cf bind-service bootcamp-customers bootcamp-customers-mysql         # 3
+
+cf start bootcamp-customers                                         # 4
---
+++ configuration/src/main/java/com/example/boot/Application.java
@@ -0,0 +1,10 @@
+package com.example.boot;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationConfiguration.class, args);
+	}
+}
