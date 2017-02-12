4550ac5621c0cfc3344a17510fd47a68b7443676
Josh Long <josh@joshlong.com>
2017-02-12 12:16:52 -0800

    up

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-TEST !!!!!!!!!!!!!!!!
 
+@EnableAsync
 @SpringBootApplication // <1>
 public class DemoApplication {
 
