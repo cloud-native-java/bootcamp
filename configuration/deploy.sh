ed73d578798761ab40f43962dd33ff6ddd6caeb7
Josh Long <josh@joshlong.com>
2017-02-12 23:21:27 -0800

    bootcamp

--- configuration/deploy.sh
+++
@@ -1,8 +0,0 @@
-cf create-service p-mysql 100mb bootcamp-customers-mysql            # 1
-
-cf push -p target/configuration.jar bootcamp-customers \
- --random-route --no-start                                          # 2
-
-cf bind-service bootcamp-customers bootcamp-customers-mysql         # 3
-
-cf start bootcamp-customers                                         # 4
--- configuration/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO CUSTOMERS (id,email) VALUES (1,'rj@cnj.com');
-INSERT INTO CUSTOMERS (id,email) VALUES (2,'ds@cnj.com');
