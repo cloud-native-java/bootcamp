f5ff845ecd4322747af35fbcf631b2876735c27f
myarbrough@oreilly.com <myarbrough@oreilly.com>
2017-06-06 06:46:17 -0700

    Edited code/bootcamp/spring-configuration/deploy.sh with Atlas code editor

--- spring-configuration/deploy.sh
+++ spring-configuration/deploy.sh
@@ -1,9 +1,9 @@
 
-cf create-service p-mysql 100mb bootcamp-customers-mysql            # 1
+cf create-service p-mysql 100mb bootcamp-customers-mysql            <1>
 
 cf push -p target/configuration.jar bootcamp-customers \
- --random-route --no-start                                          # 2
+ --random-route --no-start                                          <2>
 
-cf bind-service bootcamp-customers bootcamp-customers-mysql         # 3
+cf bind-service bootcamp-customers bootcamp-customers-mysql         <3>
 
-cf start bootcamp-customers                                         # 4
+cf start bootcamp-customers                                         <4>
